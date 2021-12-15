65d131cc1c78bb8a5a98c4a6955fb88ab1f206e6
zRitsu <74823568+zRitsu@users.noreply.github.com>
2021-12-14 21:46:50 -0300

    ytdlplayer: correção temporária devido a um update do youtube ou falha do yt-dlp.

--- utils/music/models.py
+++ utils/music/models.py
@@ -21,8 +21,10 @@ if TYPE_CHECKING:
     from ..client import BotCore
 
 
+audioformats = ["mp3", "ogg", "m4a", "webm"]
 
 YDL_OPTIONS = {
+    'format': 'bestaudio/best',
     'noplaylist': False,
     'nocheckcertificate': True,
     'ignoreerrors': False,
@@ -745,7 +747,7 @@ class YTDLPlayer(BasePlayer):
         to_run = partial(self.bot.ytdl.extract_info, url=url, download=False)
         info = await self.bot.loop.run_in_executor(None, to_run)
 
-        track.id = info['formats'][0]['url']
+        track.id = [f for f in info["formats"] if f["audio_ext"] in audioformats][0]["url"]
         return track
 
     async def process_track(self):
