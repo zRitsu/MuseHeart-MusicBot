bff8a6f70670fe7d8dab0198d50dd176291bcc92
zRitsu <74823568+zRitsu@users.noreply.github.com>
2021-12-21 03:08:38 -0300

    correção no evento de track_exception.

--- modules/music.py
+++ modules/music.py
@@ -1631,7 +1631,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
         if payload.error == "This IP address has been blocked by YouTube (429)":
             player.queue.appendleft(player.last_track)
             player.node.available = False
-            newnode = [n for n in self.bot.music.nodes if n != player.node and n.available and n.is_available]
+            newnode = [n for n in self.bot.music.nodes.values() if n != player.node and n.available and n.is_available]
             if newnode:
                 await player.change_node(newnode[0].identifier)
         else:
