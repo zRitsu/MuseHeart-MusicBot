55a3a7b8afc0c6ac2702756aa2934930fc0be3cc
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-01-10 22:25:33 -0300

    pequena correção de texto...

--- modules/misc.py
+++ modules/misc.py
@@ -63,7 +63,7 @@ class Misc(commands.Cog):
         )
 
         # resolvi add essa opção...
-        # mas caso use, por favor considere de alguma forma fornecer crédito de alguma forma :(
+        # mas caso use, por favor considere de alguma forma fornecer crédito :(
         if self.bot.config.get("HIDE_SOURCE_OWNER") != "false" and self.bot.owner.id == self.source_owner.id:
             embed.footer.text += f" | Source by: {self.source_owner}"
 
