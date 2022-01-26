19bbded2cc6f656a99d3572c15664ddd090b89ea
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-01-26 02:53:26 -0300

    typo: meus meus -> meus

--- modules/misc.py
+++ modules/misc.py
@@ -82,7 +82,7 @@ class Misc(commands.Cog):
             return
 
         embed = disnake.Embed(
-            description="Olá.\nPara ver todos os meus meus comandos use **/**\n\n"
+            description="Olá.\nPara ver todos os meus comandos use **/**\n\n"
                         f"Caso os comandos não apareçam, use o comando:\n{self.bot.default_prefix}syncguild",
             color=self.bot.get_color(guild.me)
         )
