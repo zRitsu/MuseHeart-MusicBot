70cdf582e3de91c665d0eeb716b17cc80e3710bd
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-02-04 08:11:34 -0300

    Correção na mensagem do tempo (inclusão do **(e)** de forma desnecessária em algumas ocasiões)

--- utils/music/converters.py
+++ utils/music/converters.py
@@ -237,7 +237,7 @@ def time_format(milliseconds: Union[int, float], use_names: bool = False) -> str
         strings = ", ".join(t for t in times)
 
         if last_time:
-            strings += f" e {last_time}"
+            strings += f" e {last_time}" if strings else last_time
 
     else:
 
