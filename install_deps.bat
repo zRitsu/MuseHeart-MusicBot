485d1c2f4b5f4a62e9bc540f94e59777385c015b
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-04-04 20:39:47 -0300

    forçar reinstall no update das dependências. * Evitar possiveis problemas com dependências que usam commits específicos.

--- install_deps.bat
+++ install_deps.bat
@@ -3,7 +3,7 @@ chcp 65001
 cd "%~dp0"
 if not exist "logs\" mkdir logs
 >logs\install_deps.log (
-  py -3 -m pip install -U -r requirements.txt
+  py -3 -m pip install -r requirements.txt --force-reinstall
 ) 2>&1
 
 type logs\install_deps.log
--- install_deps.sh
+++ install_deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pip3 install -U -r ./requirements.txt
+pip3 install -r ./requirements.txt --force-reinstall
