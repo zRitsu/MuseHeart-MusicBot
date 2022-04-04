6bbd4a0c3c0bc44ed25b91678483a519fb6ccb4a
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-04-04 18:24:21 -0300

    usar -m pip ao invés de pip3.

--- install_deps.bat
+++ install_deps.bat
@@ -2,7 +2,7 @@
 cd "%~dp0"
 if not exist "logs\" mkdir logs
 >logs\install_deps.log (
-  pip3 install -U -r requirements.txt
+  py -3 -m pip install -U -r requirements.txt
 ) 2>&1
 
 type logs\install_deps.log
