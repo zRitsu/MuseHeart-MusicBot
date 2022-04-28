e5ea32c75f4eb1bc9b67c98ba1614cfc1e26ade6
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-04-28 14:49:17 -0300

    Criar uma copia do .env-example -> .env no setup.bat/.sh * Apenas quando o arquivo config.json ou .env não for encontrado

--- setup.bat
+++ setup.bat
@@ -4,6 +4,7 @@ echo instalando/atualizando dependências...
 cd "%~dp0"
 if not exist ".logs\" mkdir .logs
 >.logs\setup.log (
+
   if not exist .git\ (
     git init
     attrib -h .git/
@@ -11,9 +12,18 @@ if not exist ".logs\" mkdir .logs
     git fetch origin
     git checkout -b main -f --track origin/main
   )
+
   py -3 -m venv venv
   call venv\Scripts\activate.bat
   pip install -r requirements.txt
+
+  if not exist .env (
+    if not exist config.json (
+      copy .env-example .env
+      echo Não esqueça de adicionar os tokens necessários no arquivo .env
+    )
+  )
+
 ) 2>&1
 
 type .logs\setup.log
--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
 if [ ! -d ".git" ]; then
   git init
   git remote add origin https://github.com/zRitsu/disnake-LL-music-bot.git
   git fetch origin
   git checkout -b main -f --track origin/main
 fi
+
 python3 -m venv venv
 source venv/Scripts/activate
 pip install -r ./requirements.txt
+
+if [ ! -f ".env" ] && [ ! -f "config.json" ]; then
+  cp .env-example .env
+  echo 'Não esqueça de adicionar os tokens necessários no arquivo .env'
+fi
