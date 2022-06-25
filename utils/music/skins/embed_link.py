f1320702e870ffab2f42a4d0e9e5e44750ee558c
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-06-25 16:05:24 -0300

    Pequena alteração na skin embed_link * Removido a info do volume * Deixar commandlog como uma opção normal * Alterado o estilo das dicas * Adicionado nome da playlist.

--- utils/music/skins/embed_link.py
+++ utils/music/skins/embed_link.py
@@ -3,7 +3,7 @@ from typing import Union
 import re
 import disnake
 from ..models import LavalinkPlayer, YTDLPlayer
-from ..converters import time_format
+from ..converters import time_format, fix_characters
 
 
 def load(player: Union[LavalinkPlayer, YTDLPlayer]) -> dict:
@@ -11,15 +11,15 @@ def load(player: Union[LavalinkPlayer, YTDLPlayer]) -> dict:
     txt = ""
 
     if player.current.is_stream:
-        duration_txt = f"\n🔴 **Duração:** `Livestream`"
+        duration_txt = f"\n> 🔴 **⠂Duração:** `Livestream`"
     else:
-        duration_txt = f"\n⏰ **Duração:** `{time_format(player.current.duration)}`"
+        duration_txt = f"\n> ⏰ **⠂Duração:** `{time_format(player.current.duration)}`"
 
     if player.paused:
-        txt += f"⏸️ **Em Pausa:** {player.current.uri}{duration_txt}"
+        txt += f"> ⏸️ **⠂Em Pausa:** {player.current.uri}{duration_txt}"
 
     else:
-        txt += f"▶️ **Tocando Agora:** {player.current.uri}{duration_txt} `[`<t:{int((disnake.utils.utcnow() + datetime.timedelta(milliseconds=player.current.duration - player.position)).timestamp())}:R>`]`"
+        txt += f"> ▶️ **⠂Tocando Agora:** {player.current.uri}{duration_txt} `[`<t:{int((disnake.utils.utcnow() + datetime.timedelta(milliseconds=player.current.duration - player.position)).timestamp())}:R>`]`"
 
     if not player.static:
 
@@ -27,34 +27,36 @@ def load(player: Union[LavalinkPlayer, YTDLPlayer]) -> dict:
 
     else:
 
-        txt += f"\n✋ **Pedido por:** {player.current.requester.mention}\n" \
-               f"🔊 **Volume:** `{player.volume}%`\n"
+        txt += f"\n> ✋ **⠂Pedido por:** {player.current.requester.mention}\n"
+
+        if player.current.playlist:
+            txt += f"> 📑 **⠂Playlist:** `{fix_characters(player.current.playlist['name'])}`\n"
 
         try:
-            txt += f"*️⃣ **Canal de voz:** {player.guild.me.voice.channel.mention}\n"
+            txt += f"> *️⃣ **⠂Canal de voz:** [{player.guild.me.voice.channel.mention}]\n"
         except AttributeError:
             pass
 
         if player.current.track_loops:
-            txt += f"🔂 **Repetições restantes:** `{player.current.track_loops}`\n"
+            txt += f"> 🔂 **⠂Repetições restantes:** `{player.current.track_loops}`\n"
 
         elif player.loop:
             if player.loop == 'current':
-                txt += '🔂 **Repetição:** `música atual`\n'
+                txt += '> 🔂 **⠂Repetição:** `música atual`\n'
             else:
-                txt += '🔁 **Repetição:** `fila`\n'
+                txt += '> 🔁 **⠂Repetição:** `fila`\n'
 
         if queue_size:=len(player.queue):
-            txt += f"🎼 **Músicas na fila:** `{queue_size}`\n"
+            txt += f"> 🎼 **⠂Músicas na fila:** `{queue_size}`\n"
 
     if player.command_log:
 
         log = re.sub(r"\[(.+)]\(.+\)", r"\1", player.command_log.replace("`", "")) # remover links do command_log p/ evitar gerar mais de uma preview.
 
-        txt += f"`[=================]`\n{player.command_log_emoji} **Última Interação:** {log}\n"
+        txt += f"> {player.command_log_emoji} **⠂Última Interação:** {log}\n"
 
     if player.current_hint:
-        txt += f"`[=================]`\n`💡 Dica: {player.current_hint}`"
+        txt += f"> `╔══════💡DICA💡══════╗`\n> `{player.current_hint}`"
 
     if player.auto_update:
         player.auto_update = 0
