a24302cdd761f767b04c224ece6ac7b6de884abf
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-07-07 01:36:21 -0300

    Revert "Usar devnull nos comandos de update"
    This reverts commit 4df1a4fa2217cfce3e29a5871b554ae8012d4c1a.

--- modules/legacy_cmds.py
+++ modules/legacy_cmds.py
@@ -52,11 +52,12 @@ async def run_command(cmd):
         async for x in reader:
             result.append(x)
 
-    return "\n".join(result)
+    return  "\n".join(result)
 
 
 async def run_command_old(bot: BotCore, cmd: str):
-    to_run = partial(subprocess.check_output, cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+    to_run = partial(subprocess.check_output, cmd, shell=True, stdin=None, stderr=None)
     return (await bot.loop.run_in_executor(None, to_run)).decode('utf-8').strip()
 
 
