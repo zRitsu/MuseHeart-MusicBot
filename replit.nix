3d6f8ea7c53c59c9c60cce5d34d7c1a0a11e7462
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-07-09 21:29:38 -0300

    Usar python 3.8 no replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.python310Full
+    pkgs.python38Full
     pkgs.ffmpeg
     pkgs.jdk17_headless
   ];
@@ -8,7 +8,7 @@
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.libopus
     ];
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
     LANG = "en_US.UTF-8";
   };
 }
