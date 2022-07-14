9ebbff59285ce01924e00780407f889b84bef644
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-07-13 22:00:09 -0300

    desativar PYTHONBIN do replit.nix

--- replit.nix
+++ replit.nix
@@ -8,7 +8,7 @@
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.libopus
     ];
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    #PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
     LANG = "en_US.UTF-8";
   };
 }
