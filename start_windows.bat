df10a6b2f4773b6c7a0586bcf51b49b414142afb
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-07-22 01:05:59 -0300

    Renomear run.sh para start.sh Adicionado start_windows.bat

---
+++ start_windows.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >NUL
+echo iniciando bot (verifique se ele está online)...
+@echo off
+cd "%~dp0"
+if not exist ".logs\" mkdir .logs
+
+if not exist "venv\" (
+  where py >nul 2>&1 && py -3 -m venv venv || python3 -m venv venv
+)
+
+call venv\Scripts\activate.bat
+@echo on
+python main.py 2> .logs\run.log
