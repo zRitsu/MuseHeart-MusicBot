b9630851b20bd9206af1374af62c10777b41f54d
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-07-23 06:30:01 -0300

    usar python 3.10 na repl.it

--- replit.nix
+++ replit.nix
@@ -1,10 +1,10 @@
 { pkgs }: {
   deps = [
-    pkgs.python38Full
+    pkgs.python310Full
     pkgs.adoptopenjdk-jre-bin
   ];
   env = {
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
     LANG = "en_US.UTF-8";
   };
 }
