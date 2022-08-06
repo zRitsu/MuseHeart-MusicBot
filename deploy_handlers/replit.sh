cff2726baf7e65869d19d7ecd6513bd7bddf8bc7
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-08-05 23:53:50 -0300

    Replit: Verificar por atualizações de dependências apenas quando necessário.

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -2,18 +2,25 @@
 
 [ -z "$VIDEO_PREVIEW" ] || { . venv/bin/activate && python3 preview.py; kill "$PPID"; exit 1; }
 
-if [ ! -d "venv" ] || [ ! -f "./venv/.deployed" ]; then
+if [ ! -d "venv" ] || [ ! -f "./venv/pyproject.toml" ]; then
   bash quick_update.sh
   rm -rf venv
-    echo "Inicializando virtual_env..."
+  rm poetry.lock
+  echo "Inicializando virtual_env..."
   python3 -m venv venv
   . venv/bin/activate
   python3 -m pip config unset --user install.use-feature
   python3 -m pip install -U pip poetry
-  touch ./venv/.deployed
+  echo "Instalando dependências (isso pode demorar até 5 minutos)..."
+  poetry install
+  cp -r pyproject.toml ./venv/pyproject.toml
 else
   . venv/bin/activate
+  if  ! cmp --silent -- "./pyproject.toml" "./venv/pyproject.toml"; then
+    echo "Instalando/Atualizando dependências..."
+    poetry install
+    cp -r pyproject.toml ./venv/pyproject.toml
+  fi
 fi
 
-poetry install
 python3 main.py
