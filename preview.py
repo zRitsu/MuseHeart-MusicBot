827ab4cae5b39ccd03e19e109aab1f09e44fdc14
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-08-05 22:51:33 -0300

    Remover preview.html e renderizar html diretamente do preview.py

--- preview.py
+++ preview.py
@@ -6,7 +6,17 @@ from tornado.ioloop import IOLoop
 class MainHandler(RequestHandler):
 
     def get(self):
-        self.render('./templates/preview.html', video_preview=getenv("VIDEO_PREVIEW"))
+        self.write(
+            f"""<html>
+              <head>
+              </head>
+              <body>
+                <iframe width="100%" height="100%"
+                src="{getenv('VIDEO_PREVIEW')}">
+                </iframe>
+              </body>
+            </html>"""
+        )
 
 
 app = Application([
