5e2bebc62cf19bf6e1d904af5d0c6e72efa63442
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-08-08 23:06:08 -0300

    oops...

--- quick_update.sh
+++ quick_update.sh
@@ -4,7 +4,7 @@ export PYTHONIOENCODING=utf8
 
 if [ ! -d ".git" ] || [ -z "$(git remote -v)" ]; then
   git init
-  if [ ! -z "$SOURCE_REPO" ]; then
+  if [ -z "$SOURCE_REPO" ]; then
     git remote add origin $SOURCE_REPO
   else
     git remote add origin https://github.com/zRitsu/disnake-LL-music-bot.git
