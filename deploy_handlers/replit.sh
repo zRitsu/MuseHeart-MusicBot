5fa49a43dfb3129a301adf3b047cc1f6c38d3615
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-08-16 22:04:11 -0300

    usar pip na repl.it * Evitar possíveis erros em caso de downgrade de dependências. * Ajuste nas mensagens do console.

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -2,26 +2,33 @@
 
 [ -z "$VIDEO_PREVIEW" ] || { . venv/bin/activate && python3 preview.py; kill "$PPID"; exit 1; }
 
-if [ ! -d "venv" ] || [ ! -f "./venv/pyproject.toml" ]; then
+if [ ! -d "venv" ] || [ ! -f "./venv/requirements.txt" ]; then
   bash quick_update.sh
   rm -rf venv
   rm -rf .config
   rm -rf .cache
   rm poetry.lock
-  echo "Inicializando virtual_env..."
+  echo "##################################"
+  echo "## Inicializando virtual_env... ##"
+  echo "##################################"
   python3 -m venv venv
   . venv/bin/activate
   python3 -m pip config unset --user install.use-feature
-  python3 -m pip install -U pip poetry
-  echo "Instalando dependências (Esse processo pode demorar até 5 minutos)..."
-  poetry install
-  cp -r pyproject.toml ./venv/pyproject.toml
+  python3 -m pip install -U pip
+  echo "#################################################"
+  echo "## Instalando dependências...                  ##"
+  echo "## (Esse processo pode demorar até 5 minutos). ##"
+  echo "#################################################"
+  pip3 install -U -r requirements.txt --no-cache-dir
+  cp -r requirements.txt ./venv/requirements.txt
 else
   . venv/bin/activate
-  if  ! cmp --silent -- "./pyproject.toml" "./venv/pyproject.toml"; then
-    echo "Instalando/Atualizando dependências..."
-    poetry install
-    cp -r pyproject.toml ./venv/pyproject.toml
+  if  ! cmp --silent -- "./requirements.txt" "./venv/requirements.txt"; then
+    echo "############################################"
+    echo "## Instalando/Atualizando dependências... ##"
+    echo "############################################"
+    pip3 install -U -r requirements.txt --no-cache-dir
+    cp -r requirements.txt ./venv/requirements.txt
   fi
 fi
 
