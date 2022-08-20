b8653be0da6af2c566facf0a4782476339da06db
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-08-20 20:40:27 -0300

    fix: conversão do ENABLE_LOGGER config pra bool.

--- config_loader.py
+++ config_loader.py
@@ -154,6 +154,7 @@ def load_config():
         "COMMAND_LOG",
         "RUN_RPC_SERVER",
         "AUTO_DOWNLOAD_LAVALINK_SERVERLIST",
+        "ENABLE_LOGGER"
 
         "BANS_INTENT",
         "DM_MESSAGES_INTENT",
