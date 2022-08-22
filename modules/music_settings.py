c07b3af59009c3a3bb712147189d34d906e92cff
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-08-22 20:16:04 -0300

    usar process_hint() no comando changeskin * Evitar que dicas incorretas sejam exibidas em modos/skins incompatíveis.

--- modules/music_settings.py
+++ modules/music_settings.py
@@ -597,6 +597,7 @@ class MusicSettings(commands.Cog):
             player.auto_update = 0 # linha temporária para resolver possíveis problemas com skins custom criadas por usuarios antes desse commit.
             player.controller_mode = True
             player.set_command_log(text=f"{inter.author.mention} alterou a skin do player para: **{skin}**", emoji="🎨")
+            player.process_hint()
             await player.invoke_np(force=True)
 
     @change_skin.autocomplete("skin")
