0e051b1b617672cc94a984c440e0630feb4d0b8b
zRitsu <74823568+zRitsu@users.noreply.github.com>
2022-09-03 02:03:12 -0300

    Mais um pequeno ajuste no replit.sh

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -10,6 +10,8 @@ if [ ! -d "venv" ] || [ ! -f "./venv/bin/python3" ]; then
   python3 -m venv venv
   . venv/bin/activate
   python3 -m pip config unset --user install.use-feature 2> /dev/null
+else
+  . venv/bin/activate
 fi
 
 if [ -n "${VIDEO_PREVIEW}" ]; then
@@ -26,15 +28,12 @@ elif [ ! -f "./venv/requirements.txt" ]; then
   echo "#################################################"
   pip3 install -U -r requirements.txt --no-cache-dir
   cp -r requirements.txt ./venv/requirements.txt
-else
-  . venv/bin/activate
-  if  ! cmp --silent -- "./requirements.txt" "./venv/requirements.txt"; then
-    echo "############################################"
-    echo "## Instalando/Atualizando dependências... ##"
-    echo "############################################"
-    pip3 install -U -r requirements.txt --no-cache-dir
-    cp -r requirements.txt ./venv/requirements.txt
-  fi
+elif  ! cmp --silent -- "./requirements.txt" "./venv/requirements.txt"; then
+  echo "############################################"
+  echo "## Instalando/Atualizando dependências... ##"
+  echo "############################################"
+  pip3 install -U -r requirements.txt --no-cache-dir
+  cp -r requirements.txt ./venv/requirements.txt
 fi
 
 python3 main.py
