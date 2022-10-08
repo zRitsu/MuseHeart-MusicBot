86fe086b574a6babe18c34a6be8194e534c44875
Alex <74823568+zRitsu@users.noreply.github.com>
2022-10-08 01:32:32 -0300

    fix: erro de condição...

--- modules/music.py
+++ modules/music.py
@@ -536,8 +536,7 @@ class Music(commands.Cog):
         static_player = guild_data['player_controller']
 
         if static_player['channel']:
-            if not (channel_db := bot.get_channel(int(static_player['channel']))) \
-                    or not (channel_db := bot.fetch_channel(int(static_player['channel']))):
+            if not (channel_db := bot.get_channel(int(static_player['channel'])) or bot.fetch_channel(int(static_player['channel']))):
                 await self.reset_controller_db(inter.guild_id, guild_data, inter)
             else:
                 if channel_db != channel:
