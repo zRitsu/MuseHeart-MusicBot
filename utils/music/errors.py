a4c14e45e14ed5c56832767122fc249d6c947c27
Alex <74823568+zRitsu@users.noreply.github.com>
2022-11-07 22:03:40 -0300

    printar erro completo no terminal na função parse_error

--- utils/music/errors.py
+++ utils/music/errors.py
@@ -135,6 +135,7 @@ def parse_error(
 
     if not error_txt:
         full_error_txt = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+        print(full_error_txt)
     else:
         full_error_txt = ""
 
