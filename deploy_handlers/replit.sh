771c804406b10dacc6fc8290aefe53bbbb5cd655
Alex <74823568+zRitsu@users.noreply.github.com>
2022-12-04 14:32:36 -0300

    Usar python ao invés de python3 e mover requirements.txt para diretório venv/bin

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -4,40 +4,40 @@ rm -f poetry.lock && rm -f pyproject.toml
 
 if [ -n "${VIDEO_PREVIEW}" ]; then
   . venv/bin/activate
-  python3 -m pip install tornado
-  python3 preview.py
+  python -m pip install tornado
+  python preview.py
   kill "$PPID"
   exit 1
 fi
 
-if [ ! -d "venv" ] || [ ! -f "./venv/requirements.txt" ]; then
+if [ ! -d "venv" ] || [ ! -f "./venv/bin/requirements.txt" ]; then
   rm -rf venv && rm -rf .config && rm -rf .cache && rm -rf .git
   bash quick_update.sh
   rm -f poetry.lock && rm -f pyproject.toml
   echo "##################################"
   echo "## Inicializando virtual_env... ##"
   echo "##################################"
-  python3 -m venv venv
+  python -m venv venv
   . venv/bin/activate
-  python3 -m pip install -U pip
+  python -m pip install -U pip
   echo "#################################################"
   echo "## Instalando dependências...                  ##"
   echo "## (Esse processo pode demorar até 3 minutos). ##"
   echo "#################################################"
-  pip3 install -U -r requirements.txt --no-cache-dir
-  cp -r requirements.txt ./venv/requirements.txt
+  pip install -U -r requirements.txt --no-cache-dir
+  cp -r requirements.txt ./venv/bin/requirements.txt
 
-elif ! cmp --silent -- "./requirements.txt" "./venv/requirements.txt"; then
+elif ! cmp --silent -- "./requirements.txt" "./venv/bin/requirements.txt"; then
   echo "############################################"
   echo "## Instalando/Atualizando dependências... ##"
   echo "############################################"
   . venv/bin/activate
-  pip3 install -U -r requirements.txt --no-cache-dir
-  cp -r requirements.txt ./venv/requirements.txt
+  pip install -U -r requirements.txt --no-cache-dir
+  cp -r requirements.txt ./venv/bin/requirements.txt
 
 else
   . venv/bin/activate
 
 fi
 
-python3 main.py
+python main.py
