bae70f234218ed1b19436160a029b6af6585e6b1
Alex <74823568+zRitsu@users.noreply.github.com>
2022-12-04 13:48:44 -0300

    repl.it: recriar venv ao atualizar dependências * Evitar um raro bug delas não serem instaladas ao fazer fork na repl.it

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -8,34 +8,32 @@ if [ -n "${VIDEO_PREVIEW}" ]; then
   exit 1
 fi
 
-if [ ! -d "venv" ] || [ ! -f "./venv/bin/.venv_created" ]; then
+if [ ! -d "venv" ] || [ ! -f "./venv/requirements.txt" ]; then
   rm -rf venv && rm -rf .config && rm -rf .cache
+  bash quick_update.sh
   echo "##################################"
   echo "## Inicializando virtual_env... ##"
   echo "##################################"
   python3 -m venv venv
   . venv/bin/activate
-  python3 -m pip config unset --user install.use-feature 2> /dev/null
-  touch ./venv/bin/.venv_created
-else
-  . venv/bin/activate
-fi
-
-if [ ! -f "./venv/requirements.txt" ]; then
-  bash quick_update.sh
   python3 -m pip install -U pip
   echo "#################################################"
   echo "## Instalando dependências...                  ##"
-  echo "## (Esse processo pode demorar até 5 minutos). ##"
+  echo "## (Esse processo pode demorar até 3 minutos). ##"
   echo "#################################################"
   pip3 install -U -r requirements.txt --no-cache-dir
   cp -r requirements.txt ./venv/requirements.txt
+
 elif ! cmp --silent -- "./requirements.txt" "./venv/requirements.txt"; then
   echo "############################################"
   echo "## Instalando/Atualizando dependências... ##"
   echo "############################################"
   pip3 install -U -r requirements.txt --no-cache-dir
   cp -r requirements.txt ./venv/requirements.txt
+
+else
+  . venv/bin/activate
+
 fi
 
 rm -f poetry.lock && rm -f pyproject.toml
