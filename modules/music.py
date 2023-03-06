ce8156c54e46c1814bbc8f3d7eaacca7e5124add
Alex <74823568+zRitsu@users.noreply.github.com>
2023-03-06 03:02:45 -0300

    fix: Bot processando mensagens ao criar novas threads em mensagem diferente do player (falha crítica).

--- modules/music.py
+++ modules/music.py
@@ -3520,6 +3520,9 @@ class Music(commands.Cog):
 
         if player and isinstance(message.channel, disnake.Thread) and not player.static:
 
+            if player.message.id != message.channel.id and not player.controller_mode:
+                return
+
             text_channel = message.channel
 
         else:
