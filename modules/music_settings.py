e527878bfd4f22c6b265c84d5d9f19f402e194fa
Alex <74823568+zRitsu@users.noreply.github.com>
2023-05-17 23:25:26 -0300

    fix: Verificar por permissão de ler mensagens no comando /setup

--- modules/music_settings.py
+++ modules/music_settings.py
@@ -579,6 +579,9 @@ class MusicSettings(commands.Cog):
                                    f" canal {target.mention} para me permitir gerenciar permissões, reuse o comando "
                                    f"sem selecionar um canal de destino.")
 
+            if not target.permissions_for(guild.me).read_messages:
+                raise GenericError(f"Não tenho permissão para ler mensagens no canal {target.mention}")
+
             if purge_messages == "yes":
                 await target.purge(limit=100, check=lambda m: m.author != guild.me or not m.thread)
 
