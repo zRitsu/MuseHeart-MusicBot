a660b7c9d2299a6a2c3374833c93294f557bf85c
Alex <74823568+zRitsu@users.noreply.github.com>
2023-05-20 13:02:44 -0300

    Verificar por todos os bots do pool no comando rich_presence pra verificar se o usuário é dono de um deles.

--- modules/music_settings.py
+++ modules/music_settings.py
@@ -1335,7 +1335,7 @@ class RPCCog(commands.Cog):
     )
     async def rich_presence(self, inter: disnake.AppCmdInter):
 
-        if not self.bot.config["ENABLE_RPC_COMMAND"] and not await self.bot.is_owner(inter.author):
+        if not self.bot.config["ENABLE_RPC_COMMAND"] and not any(await b.is_owner(inter.author) for b in self.bot.pool.bots):
             raise GenericError("**Este comando está desativado nas minhas configurações...**\n"
                                "Apenas o meu desenvolvedor pode ativar este comando publicamente.")
 
