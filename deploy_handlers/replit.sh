5863f9eb278c6eb614f9673065f090330cdea198
Alex <74823568+zRitsu@users.noreply.github.com>
2023-06-13 16:53:45 -0300

    Adicionar espaçamento no print de algumas etapas ao ligar o bot na repl.it

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -21,24 +21,24 @@ fi
 if [ ! -d "venv" ] || [ ! -f "./venv/bin/requirements.txt" ] || [ ! "$REPL_SLUG-$REPL_OWNER" == "$(cat ./venv/.deployed)" ]; then
   rm -rf venv && rm -rf .config && rm -rf .cache
   rm -f poetry.lock && rm -f pyproject.toml
-  echo "##################################"
-  echo "## Inicializando virtual_env... ##"
-  echo "##################################"
+  echo -e "\n####################################" \
+          "\n### Inicializando virtual_env... ###" \
+          "\n####################################\n"
   python3 -m venv venv
   . venv/bin/activate
   python3 -m pip install -U pip
-  echo "#################################################"
-  echo "## Instalando dependências...                  ##"
-  echo "## (Esse processo pode demorar até 3 minutos). ##"
-  echo "#################################################"
+  echo -e "\n###################################################" \
+          "\n### Instalando dependências...                  ###" \
+          "\n### (Esse processo pode demorar até 3 minutos). ###" \
+          "\n###################################################\n"
   pip3 install -U -r requirements.txt --no-cache-dir
   cp -r requirements.txt ./venv/bin/requirements.txt
   echo -n "$REPL_SLUG-$REPL_OWNER" > ./venv/.deployed
 
 elif ! cmp --silent -- "./requirements.txt" "./venv/bin/requirements.txt"; then
-  echo "############################################"
-  echo "## Instalando/Atualizando dependências... ##"
-  echo "############################################"
+  echo -e "\n##############################################" \
+          "\n### Instalando/Atualizando dependências... ###" \
+          "\n##############################################\n"
   . venv/bin/activate
   pip3 install -U -r requirements.txt --no-cache-dir
   cp -r requirements.txt ./venv/bin/requirements.txt
