c6c1513ed3eee482491f6a3ecfcb97017f745b26
Alex <74823568+zRitsu@users.noreply.github.com>
2023-08-02 02:18:24 -0300

    Nova skin: minimalist

---
+++ utils/music/skins/normal_player/minimalist.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import disnake
+
+from utils.music.converters import fix_characters, time_format
+from utils.music.models import LavalinkPlayer
+
+
+class Minimalist:
+
+    __slots__ = ("name", "preview")
+
+    def __init__(self):
+        self.name = "minimalist"
+        self.preview = "https://cdn.discordapp.com/attachments/554468640942981147/1136165872826847322/image.png"
+
+    def setup_features(self, player: LavalinkPlayer):
+        player.mini_queue_feature = False
+        player.controller_mode = False
+        player.auto_update = 0
+        player.hint_rate = 9
+        player.static = False
+
+    def load(self, player: LavalinkPlayer) -> dict:
+
+        duration = "🔴 Livestream" if player.current.is_stream else \
+            time_format(player.current.duration)
+
+        data = {
+            "embeds": [],
+            "content": f"`🎶⠂Tocando agora:` [`{fix_characters(player.current.title, 30)}`](<{player.current.uri or player.current.search_uri}>) `[{fix_characters(player.current.author, 20)}] {duration}`"
+        }
+
+        if player.current_hint:
+            data["content"] += f"\n`💡⠂Dica: {player.current_hint}`"
+
+        return data
+
+def load():
+    return Minimalist()
