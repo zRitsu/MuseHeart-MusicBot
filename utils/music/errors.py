f8f666acf2d8295e5c4103fdeaa2cbceeb6820dc
Alex <74823568+zRitsu@users.noreply.github.com>
2023-08-27 23:08:20 -0300

    Adicionar erro de audio não suportado no error_handler

--- utils/music/errors.py
+++ utils/music/errors.py
@@ -183,6 +183,8 @@ def parse_error(
     elif isinstance(error, WavelinkException):
         if "Unknown file format" in (wave_error := str(error)):
             error_txt = "**Não há suporte para o link especificado...**"
+        elif "No supported audio format" in wave_error:
+            error_txt = "**Não há suporte para o link informado.**"
         elif "This video is not available" in wave_error:
             error_txt = "**Este vídeo está indisponível ou privado...**"
         elif "The playlist does not exist" in wave_error:
