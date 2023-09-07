4f5d49de06c75d5f3b8cd4860cc082f7edc329a0
Alex <74823568+zRitsu@users.noreply.github.com>
2023-09-07 00:04:28 -0300

    Ignorar player com o node que está com status restarting como True

--- modules/music.py
+++ modules/music.py
@@ -5083,6 +5083,9 @@ class Music(commands.Cog):
 
                         for p in n.players.values():
 
+                            if p.node.restarting:
+                                continue
+
                             p.locked = True
 
                             p.node.restarting = True
