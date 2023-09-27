c4ece5219e941108b196662c799dacec151e82f7
Alex <74823568+zRitsu@users.noreply.github.com>
2023-09-26 23:39:49 -0300

    fix: Template/modelo manual do stage_announce não sendo aplicado em ocasiões especificas.

--- modules/music.py
+++ modules/music.py
@@ -350,29 +350,37 @@ class Music(commands.Cog):
                 raise GenericError("**Você precisa da permissão de gerenciar servidor pra usar esse comando com um modelo personalizado.\n"
                                    "Use o comando novamente sem incluir um modelo (será usado o modelo padrão).**")
 
-        if not any(p in template for p in (
-                '{track.title}', '{track.timestamp}', '{track.emoji}', '{track.author}', '{track.duration}',
-                '{track.source}', '{track.playlist}',
-                '{requester.name}', '{requester.id}'
-        )):
-            raise GenericError(
-                "**Você deve usar pelo menos um placeholder válido na mensagem.**\n\n"
-                "**PLACEHOLDERS:** ```ansi\n"
-                "[34;1m{track.title}[0m -> Nome da música\n"
-                "[34;1m{track.author}[0m -> Nome do Artista/Uploader/Author da música.\n"
-                "[34;1m{track.duration}[0m -> Duração da música.\n"
-                "[34;1m{track.timestamp}[0m -> Tempo restante da duração (apenas em canal de voz).\n"
-                "[34;1m{track.emoji}[0m -> Emoji da fonte de música (apenas em canal de voz).\n"
-                "[34;1m{track.source}[0m -> Origem/Fonte da música (Youtube/Spotify/Soundcloud etc)\n"
-                "[34;1m{track.playlist}[0m -> Nome da playlist de origem da música (caso tenha)\n"
-                "[34;1m{requester.name}[0m -> Nome/Nick do membro que pediu a música\n"
-                "[34;1m{requester.id}[0m -> ID do membro que pediu a música\n```"
-            )
+            if not any(p in template for p in (
+                    '{track.title}', '{track.timestamp}', '{track.emoji}', '{track.author}', '{track.duration}',
+                    '{track.source}', '{track.playlist}',
+                    '{requester.name}', '{requester.id}'
+            )):
 
-        if isinstance(guild.me.voice.channel, disnake.VoiceChannel):
-            template = global_data["voice_channel_status"] or "{track.emoji} {track.title} | {track.timestamp}"
+                if isinstance(inter, CustomContext):
+                    cmd = f"{inter.prefix}{inter.invoked_with}"
+                else:
+                    cmd = f"/{self.stage_announce.name}"
+
+                raise GenericError(
+                    "**Você deve usar pelo menos um placeholder válido na mensagem.**\n\n"
+                    "**PLACEHOLDERS:** ```ansi\n"
+                    "[34;1m{track.title}[0m -> Nome da música\n"
+                    "[34;1m{track.author}[0m -> Nome do Artista/Uploader/Author da música.\n"
+                    "[34;1m{track.duration}[0m -> Duração da música.\n"
+                    "[34;1m{track.timestamp}[0m -> Tempo restante da duração (apenas em canal de voz).\n"
+                    "[34;1m{track.emoji}[0m -> Emoji da fonte de música (apenas em canal de voz).\n"
+                    "[34;1m{track.source}[0m -> Origem/Fonte da música (Youtube/Spotify/Soundcloud etc)\n"
+                    "[34;1m{track.playlist}[0m -> Nome da playlist de origem da música (caso tenha)\n"
+                    "[34;1m{requester.name}[0m -> Nome/Nick do membro que pediu a música\n"
+                    "[34;1m{requester.id}[0m -> ID do membro que pediu a música```\n"
+                    "Exemplo: " + cmd + " {track.title} - {track.author}"
+                )
+
         else:
-            template = player.stage_title_template or "{track.title} | {track.author}"
+            if isinstance(guild.me.voice.channel, disnake.VoiceChannel):
+                template = global_data["voice_channel_status"] or "{track.emoji} {track.title} | {track.timestamp}"
+            else:
+                template = player.stage_title_template or "{track.title} | {track.author}"
 
         if isinstance(guild.me.voice.channel, disnake.StageChannel):
 
