88ece4245b74fdd6c64c0ebd708d07ebf1da48a2
Alex <74823568+zRitsu@users.noreply.github.com>
2023-09-29 21:04:28 -0300

    Remover os arquivos heroku_auto_update.sh e heroku_web.sh

--- deploy_handlers/heroku_auto_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bash quick_update.sh
-pip3 install -U -r requirements.txt --no-cache-dir
-python main.py
--- deploy_handlers/heroku_web.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "${SOURCE_AUTO_UPDATE,,}" == "true" ]; then
-  bash ./deploy_handlers/heroku_auto_update.sh
-else
-  python3 main.py
-fi
