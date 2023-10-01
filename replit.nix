0f20614800d7a079bafe7c3c3cafd8c91b15b6a6
Alex <74823568+zRitsu@users.noreply.github.com>
2023-10-01 00:12:16 -0300

    Revert "Atualizar arquivos relacionados a replit"
    This reverts commit c047908fce0391c2485657c6614274567dbc7fea.

--- replit.nix
+++ replit.nix
@@ -1,12 +1,22 @@
 { pkgs }: {
   deps = [
+    pkgs.python310Full
+    pkgs.replitPackages.prybar-python310
+    pkgs.replitPackages.stderred
     pkgs.jdk17_headless
-    pkgs.ffmpeg
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.glibc
+      pkgs.zlib
+      pkgs.xorg.libX11
+      pkgs.glib
       pkgs.libopus
     ];
+    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
     LANG = "en_US.UTF-8";
+    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
   };
 }
