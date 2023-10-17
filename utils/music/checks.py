e460d9f000787699517324bf5c91d42bb61c92ad
Alex <74823568+zRitsu@users.noreply.github.com>
2023-10-17 11:54:23 -0300

    fix: AttributeError ao verificar perm de enviar mensagem ao usar comando slash.

--- utils/music/checks.py
+++ utils/music/checks.py
@@ -219,7 +219,7 @@ async def check_pool_bots(inter, only_voiced: bool = False, check_player: bool =
         if only_voiced:
             continue
 
-        if not inter.channel.permissions_for(guild.me).send_messages:
+        if not bot.get_channel(inter.channel.id).permissions_for(guild.me).send_messages:
             continue
 
         if not guild.me.voice:
