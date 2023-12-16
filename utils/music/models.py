0638cc723c89471823c366c61201831986c087e7
Alex <74823568+zRitsu@users.noreply.github.com>
2023-12-16 03:38:58 -0300

    Tentar tocar a música novamente ao dar o erro java.lang.IllegalStateException: Failed to get media URL: 2000: An error occurred while decoding track token.

--- utils/music/models.py
+++ utils/music/models.py
@@ -762,12 +762,12 @@ class LavalinkPlayer(wavelink.Player):
             start_position = 0
 
             if event.cause.startswith((
+                    "java.lang.IllegalStateException: Failed to get media URL: 2000: An error occurred while decoding track token",
                     "java.net.SocketTimeoutException: Read timed out",
                     "java.net.SocketTimeoutException: Connect timed out",
                     "java.lang.IllegalArgumentException: Invalid bitrate",
                     "java.net.UnknownHostException:",
                     "java.lang.IllegalStateException: Current position is beyond this element",
-                    "javax.script.ScriptEngine.eval(String)",
                     "com.sedmelluq.discord.lavaplayer.tools.io.PersistentHttpStream$PersistentHttpException: Not success status code: 403",
             )):
 
