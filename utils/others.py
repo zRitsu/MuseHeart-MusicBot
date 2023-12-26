a4d1386370be21e38afa3a00d7bea17da6df7fca
Alex <74823568+zRitsu@users.noreply.github.com>
2023-12-26 20:13:51 -0300

    Reativar o suporte a custom status no canal de voz.

--- utils/others.py
+++ utils/others.py
@@ -744,8 +744,7 @@ def queue_track_index(inter: disnake.AppCmdInter, bot: BotCore, query: str, matc
 
     return tracklist
 
-async def update_vc_status(bot: BotCore, channel: disnake.VoiceChannel, status: str = ""):
-    return
+async def update_vc_status(bot: BotCore, channel: disnake.VoiceChannel, status: str = None):
 
     headers = {"Authorization": f"Bot {bot.http.token}"}
 
