4704b58fe52ebe74e0b2e69d9f5ea98cbbee8f6b
Alex <74823568+zRitsu@users.noreply.github.com>
2023-12-30 20:44:19 -0300

    Incluir emoji do deezer.

--- utils/others.py
+++ utils/others.py
@@ -543,6 +543,7 @@ music_source_emoji_data = {
     "youtube": "<:youtube:647253940882374656>",
     "soundcloud": "<:soundcloud:721530214764773427>",
     "spotify": "<:spotify:715717523626000445>",
+    "deezer": "<:deezer:1190802442053505025>",
     "twitch": "<:Twitch:803656463695478804>",
 }
 
