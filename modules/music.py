34664c325c395503c4ac363406379e6325cf37e5
Alex <74823568+zRitsu@users.noreply.github.com>
2024-01-01 09:42:18 -0300

    quickfix: AttributeError ao registrar nome no historico de músicas no comando play.

--- modules/music.py
+++ modules/music.py
@@ -1782,7 +1782,7 @@ class Music(commands.Cog):
                     inter.message = msg
 
                 if reg_query is not None:
-                    reg_query = {"name": tracks.name, "url": tracks.url}
+                    reg_query = {"name": tracks.title, "url": tracks.url}
 
             elif not queue_loaded:
 
