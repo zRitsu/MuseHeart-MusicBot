67b69bccdc3205381211a0a9fa7551d55857b6ab
Alex <74823568+zRitsu@users.noreply.github.com>
2024-02-25 17:06:48 -0300

    fix: Evitar o player ser destruido ao mudar de canal de voz.

--- utils/music/models.py
+++ utils/music/models.py
@@ -997,6 +997,10 @@ class LavalinkPlayer(wavelink.Player):
                 return
 
             if event.code == 4014:
+                await asyncio.sleep(1)
+                if self.guild and self.guild.me.voice:
+                    return
+                self.set_command_log(f"O player foi desligado por perca de conexão com o canal {self.last_channel.mention}...")
                 await self.destroy(force=True)
                 return
 
