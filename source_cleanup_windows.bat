15dd417655d7ced2e46f001d982d105636e4e2a7
Alex <74823568+zRitsu@users.noreply.github.com>
2024-02-27 19:00:04 -0300

    Esqueci de incluir a pasta .app_commands_sync_data...

--- source_cleanup_windows.bat
+++ source_cleanup_windows.bat
@@ -1,7 +1,7 @@
 @echo off
 del /q /f Lavalink.jar
 
-rmdir /q /s "venv" ".java" ".jabba" ".db_cache" "plugins"
+rmdir /q /s "venv" ".app_commands_sync_data" ".java" ".jabba" ".db_cache" "plugins"
 
 echo Arquivos deletados com sucesso!
 pause
