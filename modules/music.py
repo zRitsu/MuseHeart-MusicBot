9ddcc523b7fe064e944d125ee0d1e49d1c0fb2fd
Alex <74823568+zRitsu@users.noreply.github.com>
2024-03-26 01:27:14 -0300

    fix: Trocar de servidor lavalink caso ocorra o erro ao obter tracks: Video returned by YouTube isn't what was requested

--- modules/music.py
+++ modules/music.py
@@ -6783,6 +6783,11 @@ class Music(commands.Cog):
                             raise GenericError("**Não há servidores de música disponível.**")
 
                     except Exception as e:
+                        if "Video returned by YouTube isn't what was requested" in str(e):
+                            node_search.available = False
+                            await self.get_tracks(query=query, user=user, track_loops=track_loops, use_cache=use_cache,
+                                                  source=source, bot=bot)
+                            return
                         print(f"Falha ao processar busca...\n{query}\n{traceback.format_exc()}")
                         exceptions.add(repr(e))
 
