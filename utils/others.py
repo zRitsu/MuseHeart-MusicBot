5846dad885495d04b791ab515759c0a75e877458
Alex <74823568+zRitsu@users.noreply.github.com>
2024-03-26 01:28:53 -0300

    Não manter botões/select em mensagens de erros

--- utils/others.py
+++ utils/others.py
@@ -304,6 +304,7 @@ async def send_message(
     try:
         if not kwargs["components"]:
             kwargs.pop('components')
+            kwargs["view"] = None
     except KeyError:
         pass
 
