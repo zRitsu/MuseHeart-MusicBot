da026e3514fcd7e94f2a58a6b0f905491e540438
Alex <74823568+zRitsu@users.noreply.github.com>
2024-04-19 09:09:14 -0300

    fix: youtube não sendo adicionado na lista quando o suporte está ativado.

--- utils/others.py
+++ utils/others.py
@@ -393,10 +393,12 @@ async def send_idle_embed(
     for n in bot.music.nodes.values():
         try:
             for p in n.info["sourceManagers"]:
-                if p == "youtube" and ("ytsearch" not in n.original_providers and "ytmsearch" not in n.original_providers):
+                if p == "youtube":
+                    if "ytsearch" not in n.original_providers and "ytmsearch" not in n.original_providers:
+                        continue
+                elif p == "http":
                     continue
-                elif p != "http":
-                    providers.add(p)
+                providers.add(p)
         except:
             continue
 
