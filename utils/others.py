e2a921fc2eec690eee0edb6b5f0b55df6afdc8e1
Alex <74823568+zRitsu@users.noreply.github.com>
2024-05-05 03:58:51 -0300

    typo: Falta de espaço na lista de plataformas suportadas.

--- utils/others.py
+++ utils/others.py
@@ -406,7 +406,7 @@ async def send_idle_embed(
                                       ("no post" if is_forum else "no canal ou na conversa abaixo") +
                                       f" (ou clique no botão abaixo ou use o comando {cmd} aqui ou em algum outro canal)**\n\n"
                                       "**Você pode usar um nome ou um link de site compatível:**"
-                                      f" ```ansi\n[31;1mYoutube[0m, [33;1mSoundcloud[0m, [32;1mSpotify[0m, [35;1mDeezer[0m,[34;1mTwitch[0m```\n",
+                                      f" ```ansi\n[31;1mYoutube[0m, [33;1mSoundcloud[0m, [32;1mSpotify[0m, [35;1mDeezer[0m, [34;1mTwitch[0m```\n",
                           color=bot.get_color(target.guild.me))
 
     if text:
