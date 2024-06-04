1845c1039e6e8275e759cb5f749720dc65c403f2
Alex <74823568+zRitsu@users.noreply.github.com>
2024-06-04 03:29:50 -0300

    Não usar mais venv na repl.it e feito pequenas alterações após o update da mesma.

--- deploy_handlers/replit.sh
+++ deploy_handlers/replit.sh
@@ -3,37 +3,13 @@
 rm -rf poetry.lock pyproject.toml .upm
 pip3 uninstall poetry -y
 
-if [ "${SOURCE_AUTO_UPDATE,,}" == "true" ]; then
-  bash source_quick_update.sh
+if [ ! "$REPL_SLUG-$REPL_OWNER" == "$(cat ./venv/.deployed)" ]; then
+  rm -rf venv .config .cache local_database .logs Lavalink.jar application.yml plugins pyproject.toml poetry.lock
+  echo -n "$REPL_SLUG-$REPL_OWNER" > ./.deployed
 fi
 
-if [ ! -d "venv" ] || [ ! -f "./venv/bin/requirements.txt" ] || [ ! "$REPL_SLUG-$REPL_OWNER" == "$(cat ./venv/.deployed)" ]; then
-  rm -rf venv .config .cache local_database .logs Lavalink.jar application.yml plugins pyproject.toml poetry.lock
-  echo -e "\n####################################" \
-          "\n### Inicializando virtual_env... ###" \
-          "\n####################################\n"
-  python3 -m venv venv
-  . venv/bin/activate
-  python3 -m pip install -U pip
-  echo -e "\n###################################################" \
-          "\n### Instalando dependências...                  ###" \
-          "\n### (Esse processo pode demorar até 3 minutos). ###" \
-          "\n###################################################\n"
-  pip3 install -U -r requirements.txt --no-cache-dir
-  cp -r requirements.txt ./venv/bin/requirements.txt
-  echo -n "$REPL_SLUG-$REPL_OWNER" > ./venv/.deployed
-
-elif ! cmp --silent -- "./requirements.txt" "./venv/bin/requirements.txt"; then
-  echo -e "\n##############################################" \
-          "\n### Instalando/Atualizando dependências... ###" \
-          "\n##############################################\n"
-  . venv/bin/activate
-  pip3 install -U -r requirements.txt --no-cache-dir
-  cp -r requirements.txt ./venv/bin/requirements.txt
-
-else
-  . venv/bin/activate
-
+if [ "${SOURCE_AUTO_UPDATE,,}" == "true" ]; then
+  bash source_quick_update.sh
 fi
 
 python3 main.py
--- replit.nix
+++ replit.nix
@@ -1,23 +1,9 @@
-{ pkgs }: {
+{pkgs}: {
   deps = [
-    pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
-    pkgs.replitPackages.stderred
     pkgs.jdk17_headless
+    pkgs.glib
+    pkgs.libopus
+    pkgs.zlib
+    pkgs.glibc
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-      pkgs.glibc
-      pkgs.zlib
-      pkgs.xorg.libX11
-      pkgs.glib
-      pkgs.libopus
-    ];
-    PYTHONHOME = "${pkgs.python310Full}";
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
-    LANG = "en_US.UTF-8";
-    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
-  };
 }
