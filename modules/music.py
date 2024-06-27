57aa3525b0aa312fe0eedaeeb284e4826c56c5be
Alex <74823568+zRitsu@users.noreply.github.com>
2024-06-27 12:34:00 -0300

    fix: Botão de tocar agora puxando link com caracteres inválidos de hyperlink.

--- modules/music.py
+++ modules/music.py
@@ -5081,7 +5081,7 @@ class Music(commands.Cog):
                     if not (url:=interaction.message.embeds[0].author.url):
                         if not (matches:=URL_REG.findall(interaction.message.embeds[0].description)):
                             return
-                        url = matches[0].strip("<>")
+                        url = matches[0].split(">")[0]
                 except:
                     return
 
