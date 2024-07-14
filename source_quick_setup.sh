7a01534b0ffac33327c6ea40ec12e265f638382c
Batuhan <65771082+Sepultrex@users.noreply.github.com>
2024-07-14 06:37:28 +0300

    Update source_quick_setup.sh

--- source_quick_setup.sh
+++ source_quick_setup.sh
@@ -8,5 +8,5 @@ pip install -r ./requirements.txt 2>&1 | tee "./.logs/setup.log"
 
 if [ ! -f ".env" ] && [ ! -f "config.json" ]; then
   cp .example.env .env
-  echo '.env dosyasına gerekli jetonları eklemeyi unutmayın'
+  echo '.env dosyasına gerekli tokenleri eklemeyi unutmayın'
 fi
