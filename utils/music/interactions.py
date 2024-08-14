c180c9f13fe8de81ed889d94663212fd63f6c66c
Alex <74823568+zRitsu@users.noreply.github.com>
2024-08-13 22:34:12 -0300

    Limitar quantidade de caracteres exibidos nos itens do favmanager.

--- utils/music/interactions.py
+++ utils/music/interactions.py
@@ -1470,8 +1470,8 @@ class FavMenuView(disnake.ui.View):
                     name, url = data
                     e = get_source_emoji_cfg(self.bot, url)
                     if e:
-                        return f"` {index:02} ` {e} [`{name}`](<{url}>)"
-                    return f"` {index:02} ` [`{name}`](<{url}>)"
+                        return f"` {index:02} ` {e} [`{fix_characters(name, 45)}`](<{url}>)"
+                    return f"` {index:02} ` [`{fix_characters(name, 45)}`](<{url}>)"
 
                 txt += "\n".join(
                     f"> {format_fav(n+1, d)}" for n, d in enumerate(islice(self.data["fav_links"].items(), 25))
@@ -1496,8 +1496,8 @@ class FavMenuView(disnake.ui.View):
                     name, data = data
                     e = get_source_emoji_cfg(self.bot, data['url'])
                     if e:
-                        return f"` {index:02} ` {e} [`{name}`](<{data['url']}>)"
-                    return f"` {index:02} ` [`{name}`](<{data['url']}>)"
+                        return f"` {index:02} ` {e} [`{fix_characters(name, 45)}`](<{data['url']}>)"
+                    return f"` {index:02} ` [`{fix_characters(name, 45)}`](<{data['url']}>)"
 
                 txt += f"**Links atuais no bot {self.bot.user.mention}:**\n" + "\n".join(
                     f"> {format_gfav(n+1, d)}" for n, d in enumerate(islice(self.guild_data["player_controller"]["fav_links"].items(), 25))
@@ -1521,8 +1521,8 @@ class FavMenuView(disnake.ui.View):
                         url = url["url"]
                     e = get_source_emoji_cfg(bot, url)
                     if e:
-                        return f"` {index:02} ` {e} [`{name[5:]}`](<{url}>)"
-                    return f"` {index:02} ` [`{name}`](<{url}>)"
+                        return f"` {index:02} ` {e} [`{fix_characters(name[5:], 45)}`](<{url}>)"
+                    return f"` {index:02} ` [`{fix_characters(name, 45)}`](<{url}>)"
 
                 txt += f"### Suas integrações atuais:\n" + "\n".join(
                     f"> {format_itg(self.bot, n+1, d)}" for n, d in enumerate(islice(self.data["integration_links"].items(), 25)))
