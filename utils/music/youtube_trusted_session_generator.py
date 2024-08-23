2c81fdc6d05a1eda8d37eb6e775c2ac9125f7bd2
Alex <74823568+zRitsu@users.noreply.github.com>
2024-08-22 23:05:17 -0300

    Incluir arquivo youtube_trusted_session_generator.py

---
+++ utils/music/youtube_trusted_session_generator.py
@@ -0,0 +1,53 @@
+# código original obtido no repositório: https://github.com/iv-org/youtube-trusted-session-generator
+import pprint
+import traceback
+from typing import Optional
+
+import nodriver
+from nodriver import start, cdp, loop
+import json
+
+class Browser:
+
+    def __init__(self):
+        self.browser: Optional[nodriver.Browser] = None
+        self.tab: Optional[nodriver.Tab] = None
+        self.data = {}
+
+    async def main_session_gen(self):
+        self.browser = await start(headless=False)
+        print("[INFO] launching browser.")
+        self.tab = self.browser.main_tab
+        self.tab.add_handler(cdp.network.RequestWillBeSent, self.send_handler)
+        page = await self.browser.get('https://www.youtube.com/embed/jNQXAC9IVRw')
+        await self.tab.wait(cdp.network.RequestWillBeSent)
+        button_play = await self.tab.select("#movie_player")
+        await button_play.click()
+        await self.tab.wait(cdp.network.RequestWillBeSent)
+        print("[INFO] waiting additional 30 seconds for slower connections.")
+        await self.tab.sleep(30)
+
+    async def send_handler(self, event: cdp.network.RequestWillBeSent):
+        if "/youtubei/v1/player" in event.request.url:
+            post_data = event.request.post_data
+            post_data_json = json.loads(post_data)
+            self.data = {
+                "visitor_data": post_data_json["context"]["client"]["visitorData"],
+                "po_token": post_data_json["serviceIntegrityDimensions"]["poToken"]
+            }
+            self.browser.stop()
+            await self.tab.aclose()
+        return
+
+if __name__ == '__main__':
+
+    b = Browser()
+
+    try:
+        loop().run_until_complete(b.main_session_gen())
+    except ConnectionRefusedError:
+        pass
+    except:
+        traceback.print_exc()
+
+    pprint.pprint(b.data)
