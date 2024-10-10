259360210309a9710c246e4115f3f35ddc2bbea7
Alex <74823568+zRitsu@users.noreply.github.com>
2024-10-10 14:50:26 -0300

    usar singletitle na property search_url

--- utils/music/models.py
+++ utils/music/models.py
@@ -152,7 +152,7 @@ class PartialTrack:
 
     @property
     def search_uri(self):
-        return f"https://www.youtube.com/results?search_query={quote((self.author + '-' + self.title) if self.info['sourceName'] not in ('youtube', 'soundcloud') else self.title)}"
+        return f"https://www.youtube.com/results?search_query={quote((self.author + '-' + self.single_title) if self.info['sourceName'] not in ('youtube', 'soundcloud') else self.title)}"
 
     @property
     def title(self) -> str:
